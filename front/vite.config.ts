864d91e25ea79d1aa0d3ca9e798e01e6d48f477e
Niv3k El Pato <niv3k.business@gmail.com>
2024-02-29 22:46:18 +0000

    disabled cross embed policy on dev server

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -6,8 +6,8 @@ export default defineConfig({
   plugins: [react()],
   server: {
     headers: {
-      'Cross-Origin-Opener-Policy': 'same-origin',
-      'Cross-Origin-Embedder-Policy': 'require-corp'
+//      'Cross-Origin-Opener-Policy': 'same-origin',
+//      'Cross-Origin-Embedder-Policy': 'require-corp'
     }
   }
 })
